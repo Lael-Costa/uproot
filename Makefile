e97654e418af7b0d9057004136c99f0921c469a6
Lael Costa <Lael_Costa@Brown.edu>
2018-04-30 21:05:53 -0400

    shaders

--- Makefile
+++ Makefile
@@ -6,8 +6,8 @@ TARGET := bin/uproot
 SRCEXT := cpp
 SOURCES := $(shell find $(SRCDIR) -type f -name *.$(SRCEXT))
 OBJECTS := $(patsubst $(SRCDIR)/%,$(BUILDDIR)/%,$(SOURCES:.$(SRCEXT)=.o))
-CFLAGS := -g -Wall -Wno-deprecated-declarations
-LIB := -L lib -framework OpenGL -lglfw
+CFLAGS := -g -Wall -Wno-deprecated-declarations -std=c++14
+LIB := -L lib -framework OpenGL -lglfw -lglew
 INC := -I include
 
 $(TARGET): $(OBJECTS)
--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,73 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <memory>
+#include <string>
+
+#include "shader.h"
+
+static void onKeyPress(GLFWwindow *window,
+  int key,
+  int scancode,
+  int action,
+  int mods)
+{
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, GLFW_TRUE);
+  }
+}
+
+void printGLVersion()
+{
+  GLint maj = 0, min = 0;
+  glGetIntegerv(GL_MAJOR_VERSION, &maj);
+  glGetIntegerv(GL_MINOR_VERSION, &min);
+  std::cout << "OpenGL Version: " << maj << "." << min << std::endl;
+}
 
 int main(int argc, char** argv)
 {
   if (!glfwInit())
   {
-    std::cerr << "GLFW initialization failed. Uh-oh!" << std::endl;
+    std::cerr << "GLFW initialization failed!" << std::endl;
     exit(EXIT_FAILURE);
   }
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
   GLFWwindow* window = glfwCreateWindow(640, 480, "Uproot", NULL, NULL);
   if (!window)
   {
-    std::cerr << "Window creation failed. Uh-oh!" << std::endl;
+    std::cerr << "Window creation failed!" << std::endl;
     glfwTerminate();
     exit(EXIT_FAILURE);
   }
 
+  glfwSetKeyCallback(window, onKeyPress);
+
   glfwMakeContextCurrent(window);
+
+  GLenum err = glewInit();
+  if (err != GLEW_OK)
+  {
+    std::cerr << "Awww..." << std::endl;
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(EXIT_FAILURE);
+  }
+  printGLVersion();
+
+  std::string vertexShaderSource;
+  std::string fragmentShaderSource;
+
+  std::shared_ptr<Shader> shader = std::make_shared<Shader>(
+    "/Users/laelcosta/Dropbox/Spring2017/Math 154/uproot/res/shader.vs",
+    "/Users/laelcosta/Dropbox/Spring2017/Math 154/uproot/res/shader.fs");
+  shader->bind();
   glfwSwapInterval(1);
 
   while (!glfwWindowShouldClose(window))
@@ -28,10 +76,15 @@ int main(int argc, char** argv)
     glfwGetFramebufferSize(window, &width, &height);
     glViewport(0, 0, width, height);
     glClear(GL_COLOR_BUFFER_BIT);
+
+
+
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
+
   glfwDestroyWindow(window);
+
   glfwTerminate();
   exit(EXIT_SUCCESS);
 }
